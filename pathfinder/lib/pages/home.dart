f2e138ce86f2a6b8783c0ee9a5880c0ee0e45182
PrakharAgrawal30 <prakhar_a@bt.iitr.ac.in>
2025-01-07 09:31:33 +0530

    make buttons clickable

--- pathfinder/lib/pages/home.dart
+++ pathfinder/lib/pages/home.dart
@@ -1,27 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatelessWidget 
+{
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(   
-      appBar: AppBar(
-      title: Text('PathFinder',
-      style:TextStyle(
-      color: Colors.black,
-      fontSize: 28,
-      fontWeight:FontWeight.bold
-      )
-      ),
-      leading: Container( 
-        margin: EdgeInsets.all(10),
-        decoration: BoxDecoration( 
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(10)
-        )
-      )
-    )
+  Widget build(BuildContext context) 
+  {
+   return Scaffold
+   (   
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar
+    (
+     title: Text
+     (
+       'PathFinder',
+       style:TextStyle
+       (
+         color: Colors.black,
+         fontSize: 25,
+         fontWeight:FontWeight.bold
+       )
+     ),
+     leading: GestureDetector
+      ( 
+        onTap: () {
+       
+        },
+       child: Container
+       (
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset
+         (
+         'assets/icons/left-arrow-svgrepo-com.svg',
+         height: 30,
+         width: 30
+         ),
+          decoration: BoxDecoration
+         ( 
+         color: Colors.white,
+         borderRadius: BorderRadius.circular(10)
+         ),
+       )
+     ),
+     actions: 
+     [ 
+       GestureDetector
+       ( 
+         onTap: () {
+          
+         },
+
+      
+         child: Container
+         ( 
+           margin: EdgeInsets.all(10),
+           alignment: Alignment.center,
+           child: SvgPicture.asset
+           (
+             'assets/icons/dots-horizontal-svgrepo-com.svg',
+             height: 30,
+             width: 30
+           ),
+           decoration: BoxDecoration
+           ( 
+             color: Colors.white,
+             borderRadius: BorderRadius.circular(10)
+           ),
+         ),
+       ),
+     ],
     );
   }
 }
